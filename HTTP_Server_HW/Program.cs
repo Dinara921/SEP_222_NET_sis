0cd83a9976c211917c49cab2c59588929483caba
dinash <dinash@DESKTOP-S23LER7>
2024-07-15 22:30:27 +0600

    СС

---
+++ HTTP_Server_HW/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Net;
+using System.Text;
+
+namespace HTTP_Server_HW
+{
+    internal class Program
+    {
+        static Thread threawdListener;
+        static void Main(string[] args)
+        {
+            //threawdListener = new Thread(new ParameterizedThreadStart(Start));
+            //threawdListener.Start("http://localhost:12345/");
+            threawdListener = new Thread(new ParameterizedThreadStart(Start2));
+            threawdListener.Start("http://localhost:12346/");
+        }
+        public static void Start(object prefix)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(prefix.ToString());
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Прослушка работает ....");
+                HttpListenerContext context = listener.GetContext();
+
+                HttpListenerRequest request = context.Request;
+                var qry = request.RawUrl.Split('/');
+                string Segment1 = null;
+
+                Segment1 = qry[1];
+
+                int a;
+                a = int.Parse(Segment1);
+
+
+                Console.WriteLine("Получено .." + Segment1 + " ");
+                HttpListenerResponse response = context.Response;
+
+
+                string responseString = "<HTML><BODY> " + Factorial(a) + " </HTML></BODY>";
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                output.Close();
+            }
+        }
+
+        public static void Start2(object prefix)
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(prefix.ToString());
+            listener.Start();
+            while (true)
+            {
+                Console.WriteLine("Прослушка работает..");
+                HttpListenerContext context = listener.GetContext();
+
+                HttpListenerRequest request = context.Request;
+                string text;
+                int a;
+                using (var reader = new StreamReader(request.InputStream,
+                                                     request.ContentEncoding))
+                {
+                    text = reader.ReadToEnd();
+                    string[] amp = text.Split('&');
+                    string[] perem1 = amp[0].Split('=');
+                    a = int.Parse(perem1[1]);
+                }
+                HttpListenerResponse response = context.Response;
+                string responseString = "<HTML><BODY> Factorial = " + Factorial(a) + " </BODY></HTML>";
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+
+                response.ContentLength64 = buffer.Length;
+                Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+
+                output.Close();
+            }
+        }
+
+        static long Factorial(int n)
+        {
+            if (n < 0) return -1;
+            if (n == 0) return 1;
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
---
+++ RestSharpExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+
+namespace RestSharpExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Отправка GET запроса на http://localhost:12345/5
+            TestGetFactorial(5);
+
+            // Отправка POST запроса на http://localhost:12346/
+            TestPostFactorial(5);
+        }
+
+        static void TestGetFactorial(int number)
+        {
+            var client = new RestClient("http://localhost:12345/");
+            var request = new RestRequest($"{number}", Method.Get);
+            var response = client.Execute(request);
+
+            Console.WriteLine("GET Response:");
+            Console.WriteLine(response.Content);
+        }
+
+        static void TestPostFactorial(int number)
+        {
+            var client = new RestClient("http://localhost:12346/");
+            var request = new RestRequest("", Method.Post);
+            request.AddParameter("a", number);
+            var response = client.Execute(request);
+
+            Console.WriteLine("POST Response:");
+            Console.WriteLine(response.Content);
+        }
+    }
+}

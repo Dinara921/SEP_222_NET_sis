cb0033428ed7b74e86b0482e9cbdcab9d54da5f5
ЖиенбаеваД <@207-5>
2024-07-26 20:21:56 +0500

    SMTP

---
+++ MyFTP/Program.cs
@@ -0,0 +1,54 @@
+﻿using FluentFTP;
+using Renci.SshNet;
+using System.Net;
+using System.Net.Mail;
+
+namespace MyFTP
+{
+    internal class Program
+    {
+        //https://filezilla.ru/download/FileZillaPortable_3.63.2.paf.exe
+        static string url = "ftp://ftp.dlptest.com/";
+        static string user = "dlpuser";
+        static string password = "rNrKYTX9g7z3RgJRmxWuGHbeu";
+
+        static void FTP_test()
+        {
+            FtpClient client = new FtpClient();
+            client.Host = url;
+            client.Credentials = new NetworkCredential(user, password);
+            client.Connect();
+            var status = client.UploadFile("123.txt", "/07/19/123kdadkadakdaskdjasklj.txt");
+            ;
+        }
+
+        static void SFTP()
+        {
+            string Host = "test.rebex.net";
+            int Port = 22;
+            string LocalDestinationFilename = "123.txt";
+            string Username = "demo";
+            string Password = "password";
+
+            using (var sftp = new SftpClient(Host, Port, Username, Password))
+            {
+                sftp.Connect();
+
+                var files = sftp.ListDirectory("//");
+
+                foreach (var file in files)
+                {
+                    Console.WriteLine(file.Name);
+                }
+                sftp.Disconnect();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //FTP_test();
+            SFTP();
+        }
+       
+    }
+}
---
+++ MySMTP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Mail;
+
+namespace MySMTP
+{
+    internal class Program
+    {
+        ////https://help.mail.ru/mail/mailer/popsmtp
+        static void Main(string[] args)
+        {
+            MailSend.Send();
+        }
+        public class MailSend
+        {
+            public static void Send()
+            {
+                try
+                {
+                    MailMessage mail = new MailMessage();
+                    mail.IsBodyHtml = true;
+                    mail.From = new MailAddress("dinash6145@mail.ru");
+                    mail.To.Add("murat_b@mail.ru");
+                    mail.Subject = "SMTP";
+                    mail.Body = "HelloStep";
+                    using (System.Net.Mail.SmtpClient server = new System.Net.Mail.SmtpClient("smtp.mail.ru", 587))
+                    {
+                        server.UseDefaultCredentials = false;
+                        server.Credentials = new System.Net.NetworkCredential("dinash6145@mail.ru", "apFLK2f2mLBXPgyunCnn");
+                        server.EnableSsl = true;
+                        server.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        server.Send(mail);
+                        server.Dispose();
+                        Console.WriteLine("Send");
+                    }
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.Message);
+                }
+            }
+        }
+    }
+}
